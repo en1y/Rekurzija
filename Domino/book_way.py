85d093615c970bd75c6c6160e3b36050395b6149
en1y <marsentiy.cooo@gmail.com>
2024-02-23 01:12:01 +0100

    It didn't work quite well before, but now it's perfect!

--- Domino/book_way.py
+++ Domino/book_way.py
@@ -50,6 +50,13 @@ class DominoChain:
                 raise TypeError("You have to input 2 elements both of which are in between 1 and 6")
         self.free_domino_list = domino_list.copy()
         self.domino_chain = []
+        self.dominoes_num = len(self.free_domino_list)
+
+    def __repr__(self):
+        result = """"""
+        for i in self.domino_chain:
+            result += f"{i} \n"
+        return result
 
     def create_chain(self, free_dominoes_num, start=0, finish=0, possibility_num = 0):
         for i in range(free_dominoes_num):
@@ -60,38 +67,42 @@ class DominoChain:
                 self.create_chain(free_dominoes_num-1, base_domino.first_num, base_domino.second_num, possibility_num)
                 self.free_domino_list.insert(0, base_domino)
                 possibility_num += 1
+            elif len(self.domino_chain[possibility_num]) == self.dominoes_num:
+                continue
             else:
+                start = self.domino_chain[possibility_num][0].first_num
+                finish = self.domino_chain[possibility_num][len(self.domino_chain[possibility_num]) - 1].second_num
                 start_finish_domino = Dominoes(start, finish)
                 if base_domino.connectable(start_finish_domino):
                     if base_domino.connectable_left(finish):
                         self.domino_chain[possibility_num].append(base_domino)
                         self.free_domino_list.pop(i)
                         self.create_chain(free_dominoes_num - 1, start, base_domino.second_num, possibility_num)
-                        self.free_domino_list.append(base_domino)  # ?????
+                        self.free_domino_list.insert(0, base_domino)
                     elif base_domino.connectable_right(finish):
                         base_domino = base_domino.reverse()
                         self.domino_chain[possibility_num].append(base_domino)
                         self.free_domino_list.pop(i)
                         self.create_chain(free_dominoes_num - 1, start, base_domino.second_num, possibility_num)
-                        self.free_domino_list.append(base_domino.reverse())
+                        self.free_domino_list.insert(0, base_domino.reverse())
                     elif base_domino.connectable_right(start):
                         self.domino_chain[possibility_num].insert(0, base_domino)
                         self.free_domino_list.pop(i)
                         self.create_chain(free_dominoes_num - 1, base_domino.first_num, finish, possibility_num)
-                        self.free_domino_list.append(base_domino)
+                        self.free_domino_list.insert(0, base_domino)
                     else:
                         base_domino = base_domino.reverse()
                         self.domino_chain[possibility_num].insert(0, base_domino)
                         self.free_domino_list.pop(i)
                         self.create_chain(free_dominoes_num - 1, base_domino.first_num, finish, possibility_num)
-                        self.free_domino_list.append(base_domino.reverse())
-
+                        self.free_domino_list.insert(0, base_domino.reverse())
 
 
 def main():
     domino_num = int(input())
     domino_chain = DominoChain(domino_num)
     domino_chain.create_chain(len(domino_chain.free_domino_list))
-    print(domino_chain.domino_chain)
+    print(domino_chain)
+
 
 main()
