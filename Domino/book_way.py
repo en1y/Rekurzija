7b5dfa252fdd8f4aef24699c345dafe8f5079704
en1y <marsentiy.cooo@gmail.com>
2024-02-25 17:05:32 +0100

    Domino/book_way.py works

--- Domino/book_way.py
+++ Domino/book_way.py
@@ -91,44 +91,6 @@ class DominoChain:
             delete_num = secrets.choice(range(self.dominoes_num))
             self.free_domino_list.pop(delete_num)
 
-    # def create_chain(self, free_dominoes_num, start=0, finish=0, possibility_num=0):
-    #     for i in range(free_dominoes_num):
-    #         base_domino = self.free_domino_list[i]
-    #         if finish == 0:
-    #             self.domino_chain.append([base_domino])
-    #             self.free_domino_list.pop(i)
-    #             self.create_chain(free_dominoes_num - 1, base_domino.first_num, base_domino.second_num, possibility_num)
-    #             self.free_domino_list.insert(0, base_domino)
-    #             possibility_num += 1
-    #         elif len(self.domino_chain[possibility_num]) == self.dominoes_num:
-    #             continue
-    #         else:
-    #             start = self.domino_chain[possibility_num][0].first_num
-    #             finish = self.domino_chain[possibility_num][len(self.domino_chain[possibility_num]) - 1].second_num
-    #             start_finish_domino = Dominoes(start, finish)
-    #             if base_domino.connectable(start_finish_domino):
-    #                 if base_domino.connectable_left(finish):
-    #                     self.domino_chain[possibility_num].append(base_domino)
-    #                     self.free_domino_list.pop(i)
-    #                     self.create_chain(free_dominoes_num - 1, start, base_domino.second_num, possibility_num)
-    #                     self.free_domino_list.insert(0, base_domino)
-    #                 elif base_domino.connectable_right(finish):
-    #                     base_domino = base_domino.reverse()
-    #                     self.domino_chain[possibility_num].append(base_domino)
-    #                     self.free_domino_list.pop(i)
-    #                     self.create_chain(free_dominoes_num - 1, start, base_domino.second_num, possibility_num)
-    #                     self.free_domino_list.insert(0, base_domino.reverse())
-    #                 elif base_domino.connectable_right(start):
-    #                     self.domino_chain[possibility_num].insert(0, base_domino)
-    #                     self.free_domino_list.pop(i)
-    #                     self.create_chain(free_dominoes_num - 1, base_domino.first_num, finish, possibility_num)
-    #                     self.free_domino_list.insert(0, base_domino)
-    #                 else:
-    #                     base_domino = base_domino.reverse()
-    #                     self.domino_chain[possibility_num].insert(0, base_domino)
-    #                     self.free_domino_list.pop(i)
-    #                     self.create_chain(free_dominoes_num - 1, base_domino.first_num, finish, possibility_num)
-    #                     self.free_domino_list.insert(0, base_domino.reverse())
     def create_chain(self, free_dominoes_num=-1, start=0, finish=0):
         changed = False
         # "B" - base orientation, "R" - reversed orientation
